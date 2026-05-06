e76f873b139c6428e061882bed86937608a6be2a
matias-oggero <matias-oggero@hotmail.com>
2026-05-06 15:31:05 -0300

    arca first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/arca/landing/landing.component';
+import { LoginComponent } from './pages/arca/login/login.component';
+import { ServiciosComponent } from './pages/arca/servicios/servicios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'arca', component: LandingComponent},
+    {path: 'arca/login', component: LoginComponent},
+    {path: 'arca/servicios', component: ServiciosComponent},
+];
---
+++ src/app/pages/arca/servicios/servicios.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-servicios',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './servicios.component.html',
+  styleUrl: './servicios.component.scss'
+})
+export class ServiciosComponent {
+
+}
